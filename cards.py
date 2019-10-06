5e906d1ff812231b1f2283bfc9c6ffd66f84bc4a
svetockak <svetakeda2504@gmail.com>
2019-10-06 15:48:45 +0700

    added main

---
+++ cards.py
@@ -0,0 +1,171 @@
+print ("Какаого цвета карта? Черная/Красная")
+color=input()
+if color== "Красная":
+    print ("Нарисованы ли на ней сердца? Да/Нет")
+    hearts=input()
+    if hearts == "Да":
+        print("Карта больше 10? Да/Нет")
+        quantity= input()
+        if quantity== "Да":
+            print ("На карте изображена девушка? Да/Нет")
+            woman=input()
+            if woman== "Да":
+                print ("Дама черви")
+            else:
+                print ("Карта самая старшая в колоде? Да/Нет")
+                oldest= input()
+                if oldest== "Да":
+                    print ("Туз черви")
+                else:
+                    print ("Карта следует сразу за 10? Да/Нет")
+                    knave= input()
+                    if knave == "Да":
+                        print("Валет черви")
+                    else:
+                        print("Король черви")
+        else:
+            print ("Карта чётная? Да/Нет")
+            parity=input()
+            if parity == "Да":
+                print ("На какое число делится карта? 3/4/5")
+                division1=input()
+                if division1== "3":
+                    print ("Шестерка черви")
+                if division1== "4":
+                    print ("Восьмерка черви")
+                if division1 == "5":
+                    print ("Десятка черви")
+            else:
+                print ("Карта больше 8? Да/Нет")
+                between=input()
+                if between == "Да":
+                    print ("Девятка черви")
+                else:
+                    print ("Семерка черви")
+
+
+
+    else:
+        print("Карта больше 10? Да/Нет")
+        quantity2 = input()
+        if quantity2 == "Да":
+            print("На карте изображена девушка? Да/Нет")
+            woman2 = input()
+            if woman2 == "Да":
+                print("Дама буби")
+            else:
+                print("Карта самая старшая в колоде? Да/Нет")
+                oldest2 = input()
+                if oldest2 == "Да":
+                    print("Туз буби")
+                else:
+                    print("Карта следует сразу за 10? Да/Нет")
+                    knave2 = input()
+                    if knave2 == "Да":
+                        print("Валет буби")
+                    else:
+                        print("Король буби")
+        else:
+            print("Карта чётная? Да/Нет")
+            parity2 = input()
+            if parity2 == "Да":
+                print("На какое число делится карта? 3/4/5")
+                division2 = input()
+                if division2 == "3":
+                    print("Шестерка буби")
+                if division2 == "4":
+                    print("Восьмерка буби")
+                if division2 == "5":
+                    print("Десятка буби")
+            else:
+                print("Карта больше 8? Да/Нет")
+                between2 = input()
+                if between2 == "Да":
+                    print("Девятка буби")
+                else:
+                    print("Семерка буби")
+
+
+else:
+    print("Нарисованы ли на ней кресты? Да/Нет")
+    hearts3 = input()
+    if hearts3 == "Да":
+        print("Карта больше 10? Да/Нет")
+        quantity3 = input()
+        if quantity3 == "Да":
+            print("На карте изображена девушка? Да/Нет")
+            woman3 = input()
+            if woman3 == "Да":
+                print("Дама крести")
+            else:
+                print("Карта самая старшая в колоде? Да/Нет")
+                oldest3 = input()
+                if oldest3 == "Да":
+                    print("Туз крести")
+                else:
+                    print("Карта следует сразу за 10? Да/Нет")
+                    knave3 = input()
+                    if knave3 == "Да":
+                        print("Валет крести")
+                    else:
+                        print("Король крести")
+        else:
+            print("Карта чётная? Да/Нет")
+            parity3 = input()
+            if parity3 == "Да":
+                print("На какое число делится карта? 3/4/5")
+                division3 = input()
+                if division3 == "3":
+                    print("Шестерка крести")
+                if division3 == "4":
+                    print("Восьмерка крести")
+                if division3 == "5":
+                    print("Десятка крести")
+            else:
+                print("Карта больше 8? Да/Нет")
+                between3 = input()
+                if between3 == "Да":
+                    print("Девятка крести")
+                else:
+                    print("Семерка крести")
+
+
+    else:
+        print("Карта больше 10? Да/Нет")
+        quantity4 = input()
+        if quantity4 == "Да":
+            print("На карте изображена девушка? Да/Нет")
+            woman4 = input()
+            if woman4 == "Да":
+                print("Дама пики")
+            else:
+                print("Карта самая старшая в колоде? Да/Нет")
+                oldest4 = input()
+                if oldest4 == "Да":
+                    print("Туз пики")
+                else:
+                    print("Карта следует сразу за 10? Да/Нет")
+                    knave4 = input()
+                    if knave4 == "Да":
+                        print("Валет пики")
+                    else:
+                        print("Король пики")
+        else:
+            print("Карта чётная? Да/Нет")
+            parity4 = input()
+            if parity4 == "Да":
+                print("На какое число делится карта? 3/4/5")
+                division4 = input()
+                if division4 == "3":
+                    print("Шестерка пики")
+                if division4 == "4":
+                    print("Восьмерка пики")
+                if division4 == "5":
+                    print("Десятка пики")
+            else:
+                print("Карта больше 8? Да/Нет")
+                between4 = input()
+                if between4 == "Да":
+                    print("Девятка пики")
+                else:
+                    print("Семерка пики")
